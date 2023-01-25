c9ebdb25b3c7bedcd3f0c7c3ec6fe8a62e739702
Ryan Antarisa <ryanantarisara@gmail.com>
2023-01-25 15:21:45 +0800

    Add file and folder structure
    Adding backend and frontend file structure

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,9 @@
+export const schemaTypes = [testimonials, works, abouts, experiences, skills, workExperience, contact, brands]
+import abouts from "./abouts"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import testimonials from "./testimonials"
+import workExperience from "./workExperience"
+import works from "./works"
